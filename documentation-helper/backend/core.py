5683ebd435c324fe03f8f87c642cfd9ce0aedcbd
klima7 <github@mail.ukasz.com>
2025-03-06 22:46:38 +0100

    langchain docs | add memory

--- documentation-helper/backend/core.py
+++ documentation-helper/backend/core.py
@@ -1,10 +1,10 @@
 from dotenv import load_dotenv
 from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain import hub
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain import hub
 
 
 load_dotenv()
@@ -13,22 +13,26 @@ load_dotenv()
 INDEX_NAME = "langchain-doc-index"
 
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: list[tuple[str, str]]):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
     chat = ChatOpenAI(model="gpt-4o-mini", temperature=0)
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
     
     stuff_documents_chain = create_stuff_documents_chain(
         llm=chat,
         prompt=retrieval_qa_chat_prompt,
     )
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
+    )
     retrieval_qa_chain = create_retrieval_chain(
-        retriever=docsearch.as_retriever(),
+        retriever=history_aware_retriever,
         combine_docs_chain=stuff_documents_chain,
     )
     
-    result = retrieval_qa_chain.invoke({"input": query})
+    result = retrieval_qa_chain.invoke({"input": query, "chat_history": chat_history})
     return dict(
         query = result["input"],
         result=result["answer"],
--- documentation-helper/main.py
+++ documentation-helper/main.py
@@ -8,12 +8,14 @@ st.header("LangChain Udemy Course - Documentation Helper Bot")
 prompt = st.text_input("Prompt", placeholder="Enter your prompt here")
 
 
-if "user_prompt_history" not in st.session_state:
+if (
+    "user_prompt_history" not in st.session_state
+    and "chat_answers_history" not in st.session_state
+    and "chat_history" not in st.session_state
+):
     st.session_state["user_prompt_history"] = []
-    
-    
-if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
+    st.session_state["chat_history"] = []
     
     
 def create_sources_string(source_urls: set[str]) -> str:
@@ -29,13 +31,15 @@ def create_sources_string(source_urls: set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating response..."):
-        generated_response = run_llm(prompt)
+        generated_response = run_llm(prompt, chat_history=st.session_state["chat_history"])
         sources = set([doc.metadata["source"] for doc in generated_response["source_documents"]])
         formatted_response = f"{generated_response['result']}\n\n{create_sources_string(sources)}"
         
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
-    
+
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", generated_response["result"]))
     
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(st.session_state["chat_answers_history"], st.session_state["user_prompt_history"]):
